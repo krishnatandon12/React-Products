ccd08c832367ce42fbd31482dd0b6f43815d306c
krishnatandon12 <105916399+krishnatandon12@users.noreply.github.com>
2023-01-26 03:20:54 +0530

    Suspense usage

--- src/Product.js
+++ src/Product.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 
 const Product = () => {
   const [productData, setProductData] = useState([]);
@@ -10,10 +10,11 @@ const Product = () => {
     setProductData(data.products);
   };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  const Loading = () => {
+    return <div>🌀Loading</div>;
+  };
 
+  //Improve the below using ternary operator
   const colorPicker = (ratingValue) => {
     if (ratingValue >= 4.75) {
       return '#E8F5E9';
@@ -22,73 +23,76 @@ const Product = () => {
     } else return '#FFF8E1';
   };
 
-  /**
-   * ratingValue >= 4
-      ? ratingValue >= 3 && ratingValue < 4
-        ? '#FFF8E1'
-        : '#FFEBEE'
-      : '#E8F5E9';
-   */
+  useEffect(() => {
+    getData();
+  }, []);
 
+  //Implement Suspense using promise to see the delay.
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-      }}
-    >
-      {productData &&
-        productData.map((item, index) => {
-          return (
-            <div
-              className="product"
-              key={item.id}
-              style={{
-                borderRadius: '12px',
-                border: '1px solid #CCCCCC80',
-                margin: '20px',
-                maxWidth: '300px',
-              }}
-            >
+    <Suspense fallback={<Loading />}>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+        }}
+      >
+        {!productData ? (
+          <Loading />
+        ) : (
+          productData &&
+          productData.map((item, index) => {
+            return (
               <div
+                className="product"
+                key={item.id}
                 style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  position: 'relative',
+                  borderRadius: '12px',
+                  border: '1px solid #CCCCCC80',
+                  margin: '20px',
+                  maxWidth: '300px',
                 }}
               >
-                <img
-                  src={item.thumbnail}
-                  alt={item.title}
-                  height="240"
+                <div
                   style={{
-                    borderRadius: '12px',
-                    width: '100%',
-                  }}
-                />
-                <span
-                  style={{
-                    backgroundColor: colorPicker(`${item.rating}`),
-                    padding: '8px',
-                    borderRadius: '4px',
-                    position: 'absolute',
-                    right: '-4px',
-                    top: '-8px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
                   }}
                 >
-                  {item.rating}/5
-                </span>
-              </div>
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    height="240"
+                    style={{
+                      borderRadius: '12px',
+                      width: '100%',
+                    }}
+                  />
+                  <span
+                    style={{
+                      backgroundColor: colorPicker(`${item.rating}`),
+                      padding: '8px',
+                      borderRadius: '4px',
+                      position: 'absolute',
+                      right: '-4px',
+                      top: '-8px',
+                    }}
+                  >
+                    {item.rating}/5
+                  </span>
+                </div>
 
-              <div style={{ padding: '20px' }}>
-                <h1 style={{ margin: 0 }}>{item.title}</h1>
-                <h2>${item.price}</h2>
-                <p style={{ fontWeight: 200 }}>{item.description}</p>
+                <div style={{ padding: '20px' }}>
+                  <h1 style={{ margin: 0 }}>{item.title}</h1>
+                  <h2>${item.price}</h2>
+                  <p style={{ fontWeight: 200 }}>{item.description}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-    </div>
+            );
+          })
+        )}
+      </div>
+    </Suspense>
   );
 };
 
